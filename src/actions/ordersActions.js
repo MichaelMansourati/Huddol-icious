99fd1c5a30e2eb96721468189597366745bd7d6b
michael <mmansourati@gmail.com>
2018-08-07 03:16:26 -0400

    can view current order, can submit current order into past orders, can view past orders on my-orders page

--- src/actions/ordersActions.js
+++ src/actions/ordersActions.js
@@ -1,4 +1,4 @@
-import { ADD_ITEM_TO_ORDER, REMOVE_ITEM_FROM_ORDER } from './types'
+import { ADD_ITEM_TO_ORDER, REMOVE_ITEM_FROM_ORDER, SUBMIT_ORDER } from './types'
 
 export const addItemToOrder = (name, price) => (dispatch, getState)=> {
   const state = getState()
@@ -13,5 +13,14 @@ export const removeItemFromOrder = (name, price) => (dispatch, getState) => {
   dispatch({
     type: REMOVE_ITEM_FROM_ORDER,
     payload: {name, price, state}
+  })
+}
+
+export const submitOrder = (currentOrder) => (dispatch, getState) => {
+  const state = getState()
+  console.log("currentOrder: ", currentOrder)
+  dispatch({
+    type: SUBMIT_ORDER,
+    payload: {state}
   })
 }
